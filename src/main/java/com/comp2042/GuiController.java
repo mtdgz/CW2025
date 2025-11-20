3660bc32da4a706e1fb97780c1e36e7893c4ead9
jyz <mustafajyz1@gmail.com>
2025-11-20 19:48:05 +0800

    bind label test to the score property so it auto updates

--- src/main/java/com/comp2042/GuiController.java
+++ src/main/java/com/comp2042/GuiController.java
@@ -204,6 +204,7 @@ public class GuiController implements Initializable {
     }
 
     public void bindScore(IntegerProperty integerProperty) {
+        scoreLabel.textProperty().bind(integerProperty.asString());
     }
 
     public void gameOver() {
