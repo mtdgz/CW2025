5a4c9e993b5c8861effeaeabcc6677737538ebdb
jyz <mustafajyz1@gmail.com>
2025-11-20 17:43:33 +0800

    changing the order of height and width for easier understanding of code

--- src/main/java/com/comp2042/SimpleBoard.java
+++ src/main/java/com/comp2042/SimpleBoard.java
@@ -16,7 +16,7 @@ public class SimpleBoard implements Board {
     private Point currentOffset;
     private final Score score;
 
-    public SimpleBoard(int width, int height) {
+    public SimpleBoard(int height, int width) {
         this.width = width;
         this.height = height;
         currentGameMatrix = new int[width][height];
