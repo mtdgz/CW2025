fae0737aa450f959cc6741ca16bab0a41cc18f16
jyz <mustafajyz1@gmail.com>
2025-11-23 21:59:03 +0800

    bug fix

--- src/main/java/com/comp2042/ViewData.java
+++ src/main/java/com/comp2042/ViewData.java
@@ -7,7 +7,7 @@ public final class ViewData {
     private final int yPosition;
     private final int[][] nextBrickData;
 
-    public ViewData(int[][] brickData, int xPosition, int yPosition, int[][] nextBrickData) {
+    public ViewData(int[][] brickData, int xPosition, int yPosition, int[][] nextBrickData, int ghostYPosition) {
         this.brickData = brickData;
         this.xPosition = xPosition;
         this.yPosition = yPosition;
