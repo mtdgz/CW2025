4138b1dfaff9bf600f52b0bc88340a400ba0ad14
jyz <mustafajyz1@gmail.com>
2025-11-28 20:04:34 +0800

    change game page size and lock it

--- src/main/java/com/comp2042/Main.java
+++ src/main/java/com/comp2042/Main.java
@@ -21,7 +21,8 @@ public class Main extends Application {
         GuiController c = fxmlLoader.getController();
 
         primaryStage.setTitle("TetrisJFX");
-        Scene scene = new Scene(root, 300, 510);
+        Scene scene = new Scene(root, 2753, 1536);
+        primaryStage.setResizable(false);
         primaryStage.setScene(scene);
         primaryStage.show();
         new GameController(c);
