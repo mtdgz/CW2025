dce65f5166c3dc548a5d0d5f1b8e4e112f57fca5
jyz <mustafajyz1@gmail.com>
2025-11-29 20:14:07 +0800

    handling a homelayout page so the user opens a home where they can choose diffuculty, character, and start gaem

---
+++ src/main/java/com/comp2042/HomeController.java
@@ -0,0 +1,44 @@
+package com.comp2042;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+
+public class HomeController {
+    private Stage stage;
+
+    public void setStage(Stage stage){
+        this.stage = stage;
+    }
+
+    @FXML
+    private void onStartGame(){
+        try{
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/gameLayout.fxml"));
+            Parent root = loader.load();
+
+            GuiController gameController = loader.getController();
+            new GameController(gameController);
+
+            Scene gameScene = new Scene(root, 460, 510);
+            stage.setScene(gameScene);
+            stage.setTitle("Tetris Minecraft Edition");
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void onChooseDifficulty(){
+
+    }
+
+    @FXML
+    public void onChooseCharacter(){
+
+    }
+}
