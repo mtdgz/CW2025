7295e1168e65cc7219b0f1c2acc0278ee4fa3ef7
jyz <mustafajyz1@gmail.com>
2025-12-01 00:43:59 +0800

    refresh brick and player position

--- src/main/java/com/comp2042/GameController.java
+++ src/main/java/com/comp2042/GameController.java
@@ -61,6 +61,7 @@ public class GameController implements InputEventListener {
     public void createNewGame() {
         board.newGame();
         viewGuiController.refreshGameBackground(board.getBoardMatrix());
+        viewGuiController.refreshBrick(board.getViewData());
     }
 
     @Override
